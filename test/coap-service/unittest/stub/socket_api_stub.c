0870d05b4646ef713fdd79c4fadf9988b05d1a8d
Arto Kinnunen <Arto.Kinnunen@arm.com>
2017-01-03 10:54:56 +0200

    Update unit test stubs to match latest socket api (#55)

--- test/coap-service/unittest/stub/socket_api_stub.c
+++ test/coap-service/unittest/stub/socket_api_stub.c
@@ -58,28 +58,12 @@ int8_t socket_bind(int8_t socket, const ns_address_t *address)
     return socket_api_stub.int8_value;
 }
 
-int8_t socket_send(int8_t socket, uint8_t *buffer, uint16_t length)
-{
-    if( socket_api_stub.counter >= 0){
-        return socket_api_stub.values[socket_api_stub.counter--];
-    }
-
-    return socket_api_stub.int8_value;
-}
 int16_t socket_read(int8_t socket, ns_address_t *address, uint8_t *buffer, uint16_t length)
 {
     if( address ){
         memset(&address->address, 0, 16);
         address->identifier = 0;
     }
-    if( socket_api_stub.counter >= 0){
-        return socket_api_stub.values[socket_api_stub.counter--];
-    }
-
-    return socket_api_stub.int8_value;
-}
-int8_t socket_sendto(int8_t socket, ns_address_t *address, uint8_t *buffer, uint16_t length)
-{
     if( socket_api_stub.counter >= 0){
         return socket_api_stub.values[socket_api_stub.counter--];
     }
@@ -110,8 +94,7 @@ int8_t socket_getsockopt(int8_t socket, uint8_t level, uint8_t opt_name, void *o
 
     return socket_api_stub.int8_value;
 }
-
-int8_t socket_sendmsg(int8_t socket, const ns_msghdr_t *msg, int flags)
+int16_t socket_sendmsg(int8_t socket, const ns_msghdr_t *msg, int flags)
 {
     if( socket_api_stub.counter >= 0){
         return socket_api_stub.values[socket_api_stub.counter--];
